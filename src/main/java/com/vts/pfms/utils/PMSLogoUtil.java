207ac996dce3946ddd3a52331b6ce611d35b99a8
india <anil.vedts@gmail.com>
2023-10-20 11:04:05 +0530

    commit by anil

--- src/main/java/com/vts/pfms/utils/PMSLogoUtil.java
+++ src/main/java/com/vts/pfms/utils/PMSLogoUtil.java
@@ -3,17 +3,22 @@ package com.vts.pfms.utils;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.Base64;
 
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import com.ibm.icu.text.SimpleDateFormat;
+
 @Component
 public class PMSLogoUtil 
 {
 	@Value("${ApplicationFilesDrive}")
 	private String LabLogoPath;
+	SimpleDateFormat  inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	SimpleDateFormat outputFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 	
 	public String getLabLogoAsBase64String(String LabCode) throws IOException
 	{
@@ -42,6 +47,12 @@ public class PMSLogoUtil
 			System.err.println("File Not Found at Path "+LabLogoPath+"/images/lablogos/"+"lablogo"+".png");
 			return null;
 		}
+	}
+	public String formatD(String date) throws ParseException {
+		String approvedDate=outputFormat.format(inputFormat.parse(date));
+		String[] approvedDate2=approvedDate.toString().split(":");
+		String approvedDateFormatted=approvedDate2[0]+" : "+approvedDate2[1];
+		return approvedDateFormatted;
 	}
 	
 }
