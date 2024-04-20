8b0910a28b9c6c46092e3f215da4b7d45368e964
User <User@vts19>
2024-04-20 17:41:15 +0530

    commit by yeshwanth

--- src/main/java/com/vts/pfms/pfts/controller/JasperJdbcConnection.java
+++ src/main/java/com/vts/pfms/pfts/controller/JasperJdbcConnection.java
@@ -1,40 +1,59 @@
 package com.vts.pfms.pfts.controller;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.internal.SessionImpl;
+import org.exolab.castor.types.Date;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
-public class JasperConnection {
-	private static final Logger logger=LogManager.getLogger(JasperConnection.class);
-	static Connection con;
-	@Qualifier("sessionFactory")
-	private static SessionFactory sessionFactory;
+//public class JasperConnection {
+//	private static final Logger logger=LogManager.getLogger(JasperConnection.class);
+//	static Connection con;
+//	@Qualifier("sessionFactory")
+//	private static SessionFactory sessionFactory;
 //	@Qualifier("oraclesessionFactory")
 //	private SessionFactory oraclesessionFactory;
 	
-	
-	public Connection getJdbcConnection()
-	{
-		Session session = sessionFactory.openSession();
-		Connection conn=null;
-		logger.info("getting jdbc connection from hibernate");
-		try
-		{
-			SessionImpl sessionImpl = (SessionImpl) session;
-			conn = sessionImpl.connection();
-		}catch(Exception h)
-		{
-			logger.debug("exception occures");
-			h.printStackTrace();
-		}
+	@Component
+	public class JasperJdbcConnection {
 		
-		return conn;
+		 private static final Logger logger = LogManager.getLogger(JasperJdbcConnection.class);
+		 
+		 @Autowired
+		 private Environment env;
+		 
+		 public Connection GetJasperJdbcConnection() {
+			 Connection conn=null;
+			 logger.info(new Date() + "Inside GetJasperJdbcConection.htm");
+			 try {
+				 conn = DriverManager.getConnection(env.getProperty("dataset.url"),env.getProperty("dataset.username"),env.getProperty("dataset.password"));
+			 }catch (Exception e) {
+				e.printStackTrace();
+			 }
+			 
+			 return conn; 
+		 }
 	}
+//		Session session = sessionFactory.openSession();
+//		Connection conn=null;
+//		logger.info("getting jdbc connection from hibernate");
+//		try
+//		{
+//			SessionImpl sessionImpl = (SessionImpl) session;
+//			conn = sessionImpl.connection();
+//		}catch(Exception h)
+//		{
+//			logger.debug("exception occures");
+//			h.printStackTrace();
+//		}
+//		
+//		return conn;
 	
-}
+	
+	
+	
+
